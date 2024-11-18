d2a8acf0671d6fb879c0e9b2d36893ea5e42a6ed
Cauan Diniz <cauanrodiniz@gmail.com>
2024-11-18 17:04:30 -0300

    chore: replace npm to yarn

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                hostname: process.env.NEXT_PUBLIC_BASE_URL,
+            }
+        ]
+    }
+};
 
 export default withNextIntl(nextConfig);
